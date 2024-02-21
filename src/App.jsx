02d99447515fd38f4da64510d46a4e2f29a236f4
Sanjay Kapilesh Sridhar <ufoundsanjay@gmail.com>
2024-02-21 18:32:53 +0000

    Base Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import NumberDisplayer from "./components/NumberDisplayer.jsx";
 
 function App() {
 
-  const [numbersCollection, setNumbersCollection] = useState([])
+  const [numbersCollection, setNumbersCollection] = useState([]);
+  const [numInput, setNumInput] = useState('')
+
+  const handleAddNumber = (number) => {
+    setNumbersCollection(prevNumbers => [...prevNumbers, parseInt(number)])
+    setNumInput("")
+  }
+
+  const totalSum = useMemo(() => {
+    // Sum all the numbers in the array
+    return numbersCollection.reduce((acc, num) => acc + num, 0);
+  }, [numbersCollection])
 
   return (
     <>
       <div>
-        <input type="number" />
-        <button>Add</button>
+        <input value={numInput} onChange={(event) => setNumInput(event.target.value)} type="number" />
+        <button onClick={() => handleAddNumber(numInput)} >Add</button>
         {numbersCollection.map(number => <NumberDisplayer key={number}  number={number}/>)}
+
+        <p>Sum: {totalSum}</p>
       </div>
     </>
   )
